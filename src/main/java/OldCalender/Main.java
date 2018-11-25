950783507ef7eedfd6c92fd41cfceeaa1ab40533
OAALILI <oussama.aalili@outlook.com>
2018-11-25 18:35:27 +0100

    Uitleg toegevoegd aan de OldCalender package

--- src/main/java/OldCalender/Main.java
+++ src/main/java/OldCalender/Main.java
@@ -7,77 +7,50 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
 
-// Eerst wat ik heb gedaan is zorgen dat ik een goeie start datum heb 
+// Eerst wat ik heb gedaan is zorgen dat ik een goeie start datum heb om vanuit daaruit te kunnen optellen
+        //Ik heb het boek gelezen en op het gedeelte Localdate en dat hielp om de opdracht te maken
         LocalDate startDate = LocalDate.of(2018, Month.JANUARY, 1);
-        // System.out.println(startDate.lengthOfMonth());
 
-//        for (int i = 1; i <= startDate.lengthOfMonth(); i++) {
-//            System.out.println(i);
-//        }
+     // hiermee kon ik de lengte van een maand uitprinten dit kon ik goed gebruiken om alle dagen van een maand uit te printen
+        System.out.println(startDate.lengthOfMonth());
+
+        //daarna ehb ik gezocht hoe ik al dagen van een maand kan uit printen, hier heb ik een loop voor gebruikt
+        for (int i = 1; i <= startDate.lengthOfMonth(); i++) {
+            System.out.println(i);
+        }
 
 
-         //maanden en dagen
+         //met deze loop ik door de maanden van het jaar en print ik ze ook
         for (int i = 0; i < 12; i++) {
             System.out.println(startDate.plusMonths(i).getMonth());
 
         }
         // loop door week
-//        for (int i = 0; i < 7; i++) {
-//            //System.out.println(startDate.plusMonths(i).getMonth());
-//            System.out.println(startDate.getDayOfWeek().plus(i));
-//        }
+        for (int i = 0; i < 7; i++) {
+            //System.out.println(startDate.plusMonths(i).getMonth());
+            System.out.println(startDate.getDayOfWeek().plus(i));
+        }
 
 
-// loop door dagen van de week afgekort
+    // loop door dagen van de week afgekort naam
                 for (int i = 0; i < 7; i++) {
-           // System.out.println(startDate.plusMonths(i).getMonth());
-
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE");
                     System.out.print(formatter.format(startDate.getDayOfWeek().plus(i))+" ");
-                  //  System.out.print(startDate.getDayOfWeek().plus(i)+" ");
                 }
-        System.out.println();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE");
-//        System.out.println(formatter.format(startDate));
 
-//        // maanden en
-//        for (int i = 0; i < 12; i++) {
-//            System.out.println(startDate.plusMonths(i).getMonth());
-//            // korte dagen
-//            for (int j = 0; j < 7; j++) {
-//                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE");
-//                    System.out.print(formatter.format(startDate.getDayOfWeek().plus(j))+" ");
-//                }
-//            System.out.println();
-//                // hier dagen doen
-//            for (int k = 1; k <= startDate.plusMonths(i).lengthOfMonth(); k++) {
-//                System.out.print(k + " ");
-//            }
-//            System.out.println();
-//        }
-        // maanden en
-//        for (int i = 0; i < 12; i++) {
-//            System.out.println(startDate.plusMonths(i).getMonth());
-//            // korte dagen
-//            for (int j = 0; j < 7; j++) {
-//                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE");
-//                    System.out.print(formatter.format(startDate.getDayOfWeek().plus(j))+" ");
-//                }
-//            System.out.println();
-//                // hier dagen doen
-//            for (int k = 1; k <= startDate.plusMonths(i).lengthOfMonth(); k++) {
 //
-//                System.out.print(k + " ");
-//            }
-//            System.out.println();
-//        }
-//        for (int k = 1; k <= startDate.lengthOfMonth(); k++) {
-
-        for (int k = 1; k <= startDate.lengthOfMonth(); k++) {
-
-            //System.out.print(k);
+//        // maanden uiprinten met daar onder de week
+        for (int i = 0; i < 12; i++) {
+            System.out.println(startDate.plusMonths(i).getMonth());
+            // korte dagen
+            for (int j = 0; j < 7; j++) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE");
+                System.out.print(formatter.format(startDate.getDayOfWeek().plus(j)) + " ");
+            }
+            System.out.println();
         }
-// dagen van de maanden uitgeprint
+
+// dagen van de maand Jan uitgeprint met daarboven de weekdagen afgerkort
         int getal = 1;
         int[][] twoD = new int[6][7];
         for (int i = 0; i < twoD.length; i++) {
@@ -87,7 +60,6 @@ public class Main {
                     twoD[i][j] = getal++;
 
                 }
-                //print 2 dim array uit
                 if (getal <= 10) {
                     System.out.print(" " + twoD[i][j] + " ");
                 } else if (twoD[i][j] == 0) {
@@ -98,6 +70,5 @@ public class Main {
             }
             System.out.println();
         }
-        System.out.println(startDate.getDayOfWeek());
     }
 }
